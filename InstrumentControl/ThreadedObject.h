39cae9e8d5b3423ede20a1464ca32c22a458d4e5
Sean Warren <sean.warren09@imperial.ac.uk>
2015-01-07 21:42:37 +0000

    Allow several ThreadedObjects to share a thread

--- InstrumentControl/ThreadedObject.h
+++ InstrumentControl/ThreadedObject.h
@@ -9,19 +9,27 @@ public:
    ThreadedObject(QObject* parent = 0)
    {
       thread = new QThread(this);
+      ConnectSignals();
 
       if (parent != 0)
          connect(parent, &QObject::destroyed, this, &QObject::deleteLater);
+   }
 
-      connect(thread, &QThread::started, this, &ThreadedObject::Init);
-      connect(this, &QObject::destroyed, thread, &QThread::quit);
-      connect(thread, &QThread::finished, thread, &QThread::deleteLater);
+   ThreadedObject(QThread* thread)
+      : thread(thread)
+   {
+      ConnectSignals();
    }
 
    void StartThread()
    {
       this->moveToThread(thread);
       thread->start();
+   }
+
+   QThread* GetThread()
+   {
+      return thread;
    }
 
    virtual ~ThreadedObject()
@@ -32,6 +40,16 @@ public:
 
    virtual void Init() {};
 
+
 protected:
    QThread* thread;
+
+private:
+   void ConnectSignals()
+   {
+      connect(thread, &QThread::started, this, &ThreadedObject::Init);
+      connect(this, &QObject::destroyed, thread, &QThread::quit);
+      connect(thread, &QThread::finished, thread, &QThread::deleteLater);
+   }
 };
+
