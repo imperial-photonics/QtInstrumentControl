73ff42c5a2a97b8b61a06b574e8b3ed32f377580
Sean Warren <s.warren@garvan.org.au>
2016-03-31 17:56:49 +1100

    Add ability to make bound controls transient

--- InstrumentControlUI/ControlBinder.h
+++ InstrumentControlUI/ControlBinder.h
@@ -22,7 +22,7 @@ class BoundControl
    // T : class of data used by widget
 public:
    template<class W, class V, class U, class T>
-   BoundControl(ControlBinder* binder, QString name, W* widget, V* obj, void(U::*setter)(T), T(U::*getter)(void), void (U::*signal)(T) = nullptr, Qt::ConnectionType connection_type = Qt::AutoConnection)
+   BoundControl(ControlBinder* binder, QString name, W* widget, V* obj, void(U::*setter)(T), T(U::*getter)(void), void (U::*signal)(T) = nullptr, Qt::ConnectionType connection_type = Qt::AutoConnection, bool transient = false)
    {
       auto widget_signal = static_cast<void (W::*)(T)>(&W::valueChanged);
       auto widget_setter = static_cast<void (W::*)(T)>(&W::setValue);
@@ -37,7 +37,7 @@ struct BoundControl<QCheckBox, V, U, T>
 {
 public:
    template<class V, class U, class T>
-   BoundControl(ControlBinder* binder, QString name, QCheckBox* widget, V* obj, void(U::*setter)(T), T(U::*getter)(void), void (U::*signal)(T) = nullptr, Qt::ConnectionType connection_type = Qt::AutoConnection)
+   BoundControl(ControlBinder* binder, QString name, QCheckBox* widget, V* obj, void(U::*setter)(T), T(U::*getter)(void), void (U::*signal)(T) = nullptr, Qt::ConnectionType connection_type = Qt::AutoConnection, bool transient = false)
    {
       auto widget_signal = static_cast<void (QCheckBox::*)(T)>(&QCheckBox::toggled);
       auto widget_setter = static_cast<void (QCheckBox::*)(T)>(&QCheckBox::setChecked);
@@ -52,7 +52,7 @@ struct BoundControl<QGroupBox, V, U, T>
 {
 public:
    template<class V, class U, class T>
-   BoundControl(ControlBinder* binder, QString name, QGroupBox* widget, V* obj, void(U::*setter)(T), T(U::*getter)(void), void (U::*signal)(T) = nullptr, Qt::ConnectionType connection_type = Qt::AutoConnection)
+   BoundControl(ControlBinder* binder, QString name, QGroupBox* widget, V* obj, void(U::*setter)(T), T(U::*getter)(void), void (U::*signal)(T) = nullptr, Qt::ConnectionType connection_type = Qt::AutoConnection, bool transient = false)
    {
       auto widget_signal = static_cast<void (QGroupBox::*)(T)>(&QGroupBox::toggled);
       auto widget_setter = static_cast<void (QGroupBox::*)(T)>(&QGroupBox::setChecked);
@@ -67,7 +67,7 @@ struct BoundControl<QLineEdit, V, U, const QString&>
 {
 public:
    template<class V, class U>
-   BoundControl(ControlBinder* binder, QString name, QLineEdit* widget, V* obj, void(U::*setter)(const QString&), const QString&(U::*getter)(void), void (U::*signal)(const QString&) = nullptr, Qt::ConnectionType connection_type = Qt::AutoConnection)
+   BoundControl(ControlBinder* binder, QString name, QLineEdit* widget, V* obj, void(U::*setter)(const QString&), const QString&(U::*getter)(void), void (U::*signal)(const QString&) = nullptr, Qt::ConnectionType connection_type = Qt::AutoConnection, bool transient = false)
    {
       auto widget_signal = static_cast<void (QLineEdit::*)(const QString&)>(&QLineEdit::textEdited);
       auto widget_setter = static_cast<void (QLineEdit::*)(const QString&)>(&QLineEdit::setText);
@@ -83,7 +83,7 @@ struct BoundControl<QComboBox, V, U, T>
 {
 public:
    template<class V, class U, class T>
-   BoundControl(ControlBinder* binder, QString name, QComboBox* widget, V* obj, void(U::*setter)(T), T(U::*getter)(void), void (U::*signal)(T) = nullptr, Qt::ConnectionType connection_type = Qt::AutoConnection)
+   BoundControl(ControlBinder* binder, QString name, QComboBox* widget, V* obj, void(U::*setter)(T), T(U::*getter)(void), void (U::*signal)(T) = nullptr, Qt::ConnectionType connection_type = Qt::AutoConnection, bool transient = false)
    {
       auto widget_signal = static_cast<void (QComboBox::*)(int)>(&QComboBox::currentIndexChanged);
       auto widget_setter = static_cast<void (QComboBox::*)(int)>(&QComboBox::setCurrentIndex);
@@ -134,33 +134,62 @@ public:
 protected:
 
    template<class W, class V, class U, class T>
-   void BindImpl(QString name, W* widget, V* obj, void(U::*setter)(T), T(U::*getter)(void), void (U::*signal)(T) = nullptr)
+   void BindImpl(QString name, W* widget, V* obj, void(U::*setter)(T), T(U::*getter)(void), void (U::*signal)(T) = nullptr, bool transient = false)
    {
-      BoundControl<W, V, U, T>* control = new BoundControl<W, V, U, T>(this, name, widget, obj, setter, getter, signal);
+      BoundControl<W, V, U, T>* control = new BoundControl<W, V, U, T>(this, name, widget, obj, setter, getter, signal, Qt::AutoConnection, transient);
    }
 
    template<class W, class V, class U, class T>
-   void DirectBindImpl(QString name, W* widget, V* obj, void(U::*setter)(T), T(U::*getter)(void), void (U::*signal)(T) = nullptr)
+   void BindImpl(QString name, W* widget, V* obj, void(U::*setter)(T), T(U::*getter)(void), bool transient)
    {
-      BoundControl<W, V, U, T>* control = new BoundControl<W, V, U, T>(this, name, widget, obj, setter, getter, signal, Qt::DirectConnection);
+      void (U::*signal)(T) = nullptr
+      BoundControl<W, V, U, T>* control = new BoundControl<W, V, U, T>(this, name, widget, obj, setter, getter, signal, Qt::AutoConnection, transient);
+   }
+
+
+   template<class W, class V, class U, class T>
+   void DirectBindImpl(QString name, W* widget, V* obj, void(U::*setter)(T), T(U::*getter)(void), void (U::*signal)(T) = nullptr, bool transient = false)
+   {
+      BoundControl<W, V, U, T>* control = new BoundControl<W, V, U, T>(this, name, widget, obj, setter, getter, signal, Qt::DirectConnection, transient);
    }
 
    template<class W, class V, class U, class T>
-   void QueuedBindImpl(QString name, W* widget, V* obj, void(U::*setter)(T), T(U::*getter)(void), void (U::*signal)(T) = nullptr)
+   void DirectBindImpl(QString name, W* widget, V* obj, void(U::*setter)(T), T(U::*getter)(void), bool transient)
    {
-      BoundControl<W, V, U, T>* control = new BoundControl<W, V, U, T>(this, name, widget, obj, setter, getter, signal, Qt::QueuedConnection);
+      void (U::*signal)(T) = nullptr
+      BoundControl<W, V, U, T>* control = new BoundControl<W, V, U, T>(this, name, widget, obj, setter, getter, signal, Qt::DirectConnection, transient);
+   }
+
+   template<class W, class V, class U, class T>
+   void QueuedBindImpl(QString name, W* widget, V* obj, void(U::*setter)(T), T(U::*getter)(void), void (U::*signal)(T) = nullptr, bool transient = false)
+   {
+      BoundControl<W, V, U, T>* control = new BoundControl<W, V, U, T>(this, name, widget, obj, setter, getter, signal, Qt::QueuedConnection, transient);
+   }
+
+   template<class W, class V, class U, class T>
+   void QueuedBindImpl(QString name, W* widget, V* obj, void(U::*setter)(T), T(U::*getter)(void), bool transient)
+   {
+      void (U::*signal)(T) = nullptr;
+      BoundControl<W, V, U, T>* control = new BoundControl<W, V, U, T>(this, name, widget, obj, setter, getter, signal, Qt::QueuedConnection, transient);
    }
 
    template<class V, class U>
-   void FilenameBindImpl(QString name, QLineEdit* widget, QPushButton* button, QString filter, V* obj, void(U::*setter)(const QString&), const QString&(U::*getter)(void), void (U::*signal)(const QString&) = nullptr)
+   void FilenameBindImpl(QString name, QLineEdit* widget, QPushButton* button, QString filter, V* obj, void(U::*setter)(const QString&), const QString&(U::*getter)(void), void (U::*signal)(const QString&) = nullptr, bool transient = false)
    {
+      BoundFilenameControl<V, U>* control = new BoundFilenameControl<V, U>(this, name, widget, button, filter, obj, setter, getter, signal);
+   }
+
+   template<class V, class U>
+   void FilenameBindImpl(QString name, QLineEdit* widget, QPushButton* button, QString filter, V* obj, void(U::*setter)(const QString&), const QString&(U::*getter)(void), bool transient = false)
+   {
+      void (U::*signal)(const QString&) = nullptr;
       BoundFilenameControl<V, U>* control = new BoundFilenameControl<V, U>(this, name, widget, button, filter, obj, setter, getter, signal);
    }
 
 private:
 
    template<class W, class U, class T1, class T2>
-   void BindWidget(QString name, W* widget, void(W::*widget_setter)(T1), void (W::*widget_signal)(T1), U* obj, void(U::*setter)(T2), T2(U::*getter)(void), void (U::*signal)(T2) = nullptr, Qt::ConnectionType connection_type = Qt::AutoConnection)
+   void BindWidget(QString name, W* widget, void(W::*widget_setter)(T1), void (W::*widget_signal)(T1), U* obj, void(U::*setter)(T2), T2(U::*getter)(void), void (U::*signal)(T2) = nullptr, Qt::ConnectionType connection_type = Qt::AutoConnection, bool transient = false)
    {
       // Connect the widget to the object
       parent->connect(widget, widget_signal, obj, setter, connection_type);
@@ -171,13 +200,16 @@ private:
    }
 
    template<class W, class U, class T1, class T2>
-   void SetByValue(QString name, W* widget, void(W::*widget_setter)(T1), void (W::*widget_signal)(T1), U* obj, void(U::*setter)(T2), T2(U::*getter)(void), void (U::*signal)(T2) = nullptr)
+   void SetByValue(QString name, W* widget, void(W::*widget_setter)(T1), void (W::*widget_signal)(T1), U* obj, void(U::*setter)(T2), T2(U::*getter)(void), void (U::*signal)(T2) = nullptr, bool transient = false)
    {
       // Save widget value on change
-      parent->connect(widget, widget_signal,
-         [this, name](T1 value) {
-         this->settings->setValue(name, QVariant::fromValue(value));
-      });
+      if (!transient)
+      {
+         parent->connect(widget, widget_signal,
+            [this, name](T1 value) {
+            this->settings->setValue(name, QVariant::fromValue(value));
+         });
+      }
 
 
       // Check if settings file contains an entry for this value
@@ -186,24 +218,29 @@ private:
       T2 v = std::bind(getter, obj)();
       QVariant default_v = QVariant::fromValue(v);
       
-      T2 v1 = (settings->value(name, default_v)).value<T2>();
-
+      if (!transient)
+         v = (settings->value(name, default_v)).value<T2>();
+      
       //std::bind(setter, obj, std::placeholders::_1)(v1);
-      std::bind(widget_setter, widget, std::placeholders::_1)(v1);
+      std::bind(widget_setter, widget, std::placeholders::_1)(v);
       
       // Force widget to emit signal
-      std::bind(widget_signal, widget, std::placeholders::_1)(v1);
+      std::bind(widget_signal, widget, std::placeholders::_1)(v);
 
    }
 
    template<class W, class U, class T>
-   void SetByReference(QString name, W* widget, void(W::*widget_setter)(const T&), void (W::*widget_signal)(const T&), U* obj, void(U::*setter)(const T&), const T&(U::*getter)(void), void (U::*signal)(const T&) = nullptr)
+   void SetByReference(QString name, W* widget, void(W::*widget_setter)(const T&), void (W::*widget_signal)(const T&), U* obj, void(U::*setter)(const T&), const T&(U::*getter)(void), void (U::*signal)(const T&) = nullptr, bool transient = false)
    {
       // Save widget value on change
-      parent->connect(widget, widget_signal,
-         [this, name](T value) {
-         this->settings->setValue(name, QVariant::fromValue(value));
-      });
+      if (!transient)
+      {
+         // Save widget value on change
+         parent->connect(widget, widget_signal,
+            [this, name](T value) {
+            this->settings->setValue(name, QVariant::fromValue(value));
+         });
+      }
 
 
       // Check if settings file contains an entry for this value
@@ -211,12 +248,12 @@ private:
       // value already in the object
       T v = std::bind(getter, obj)();
       QVariant default_v = QVariant::fromValue(v);
-
-      T v1 = (settings->value(name, default_v)).value<T>();
-
-
-      std::bind(setter, obj, std::placeholders::_1)(v1);
-      std::bind(widget_setter, widget, std::placeholders::_1)(v1);
+      
+      if (!transient)
+         v = (settings->value(name, default_v)).value<T>();
+      
+      std::bind(setter, obj, std::placeholders::_1)(v);
+      std::bind(widget_setter, widget, std::placeholders::_1)(v);
    }
 
    QObject* parent;
