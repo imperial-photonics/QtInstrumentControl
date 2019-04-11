21dd6803c44411b9d0746da2da3afad7ffba266c
Sean Warren <s.warren@garvan.org.au>
2019-04-11 18:16:14 +1000

    Add LED indicato

---
+++ InstrumentControlUI/QLedIndicator.cpp
@@ -0,0 +1,102 @@
+/***************************************************************************
+ *   Copyright (C) 2010 by Tn                                              *
+ *   thenobody@poczta.fm                                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <QPainter>
+
+#include "qledindicator.h"
+
+const qreal QLedIndicator::scaledSize = 1000; /* Visual Studio static const mess */
+
+QLedIndicator::QLedIndicator(QWidget *parent) : QWidget(parent)
+{
+    setMinimumSize(24,24);
+    okColor = { QColor(0,255,0), QColor(0,192,0) };
+    warningColor = { QColor(0,28,0), QColor(0,128,0) };
+    criticalColor = { QColor(0,28,0), QColor(0,128,0) };
+}
+
+void QLedIndicator::setStatus(LedStatus ledStatus_)
+{
+   ledStatus = ledStatus_;
+   update();
+}
+
+LedStatus QLedIndicator::getStatus()
+{
+   return ledStatus;
+}
+
+void QLedIndicator::resizeEvent(QResizeEvent *event) {
+    update();
+}
+
+void QLedIndicator::paintEvent(QPaintEvent *event) {
+    qreal realSize = qMin(width(), height());
+
+    QRadialGradient gradient;
+    QPainter painter(this);
+    QPen     pen(Qt::black);
+             pen.setWidth(1);
+
+    painter.setRenderHint(QPainter::Antialiasing);
+    painter.translate(width()/2, height()/2);
+    painter.scale(realSize/scaledSize, realSize/scaledSize);
+
+    gradient = QRadialGradient (QPointF(-500,-500), 1500, QPointF(-500,-500));
+    gradient.setColorAt(0, QColor(224,224,224));
+    gradient.setColorAt(1, QColor(28,28,28));
+    painter.setPen(pen);
+    painter.setBrush(QBrush(gradient));
+    painter.drawEllipse(QPointF(0,0), 500, 500);
+
+    gradient = QRadialGradient (QPointF(500,500), 1500, QPointF(500,500));
+    gradient.setColorAt(0, QColor(224,224,224));
+    gradient.setColorAt(1, QColor(28,28,28));
+    painter.setPen(pen);
+    painter.setBrush(QBrush(gradient));
+    painter.drawEllipse(QPointF(0,0), 450, 450);
+
+    painter.setPen(pen);
+    QPair<QColor, QColor> color;
+    if (ledStatus == LedOK)
+       color = okColor;
+    else if (ledStatus == LedWarning)
+       color = warningColor;
+    else if (ledStatus == LedCritical)
+       color = criticalColor;
+    
+    gradient = QRadialGradient(QPointF(-500, -500), 1500, QPointF(-500, -500));
+    gradient.setColorAt(0, color.first);
+    gradient.setColorAt(1, color.second);
+
+    /*if( isChecked() ) {
+        gradient = QRadialGradient (QPointF(-500,-500), 1500, QPointF(-500,-500));
+        gradient.setColorAt(0, onColor1);
+        gradient.setColorAt(1, onColor2);
+    
+    } else {
+        gradient = QRadialGradient (QPointF(500,500), 1500, QPointF(500,500));
+        gradient.setColorAt(0, offColor1);
+        gradient.setColorAt(1, offColor2);
+    }*/
+    painter.setBrush(gradient);
+    painter.drawEllipse(QPointF(0,0), 400, 400);
+}
+
---
+++ InstrumentControlUI/QLedIndicator.h
@@ -0,0 +1,54 @@
+/***************************************************************************
+ *   Copyright (C) 2010 by Tn                                              *
+ *   thenobody@poczta.fm                                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef QLEDINDICATOR_H
+#define QLEDINDICATOR_H
+
+#include <QAbstractButton>
+#include <QResizeEvent>
+#include <QColor>
+#include <QDebug>
+
+enum LedStatus
+{
+   LedOK       = 0,
+   LedWarning  = 1,
+   LedCritical = 2
+};
+
+class QLedIndicator : public QWidget
+{
+    Q_OBJECT
+    public:
+        QLedIndicator(QWidget *parent);
+        void setStatus(LedStatus ledStatus);
+        LedStatus getStatus();
+    protected:
+        virtual void paintEvent (QPaintEvent *event);
+        virtual void resizeEvent(QResizeEvent *event);
+
+    private:
+        static const qreal scaledSize;  /* init in cpp */
+        QPair<QColor, QColor> okColor, warningColor, criticalColor;
+        QPixmap ledBuffer;
+        LedStatus ledStatus = LedOK;
+};
+
+#endif // QLEDINDICATOR_H
